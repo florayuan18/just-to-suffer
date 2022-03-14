7b4252f3931f9f6c9ae67d82e073c9df98909295
florayuan18 <flora.yuan18@gmail.com>
2022-03-14 01:55:45 -0700

    challenges

--- challenges/AltMenu.java
+++ challenges/AltMenu.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 import java.util.concurrent.RunnableFuture;
 
 public class AltMenu {
-    String title;
-    Runnable action;
+    private String title; // numerical
+    private Runnable action; // changed class variable to be private
 
+    // constructor
     public AltMenu(String title, Runnable action) {
         this.title = title;
         this.action = action;
     }
 
+    // getter 1 - return the value
     public String getTitle() {
         return this.title;
     }
 
+    // getter 2 - return the value
     public Runnable getAction() {
         return this.action;
     }
@@ -23,20 +26,23 @@ public class AltMenu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Map<Integer, AltMenu> menu = new HashMap<>(); // using Map
+        Map<Integer, AltMenu> menu = new HashMap<>(); // like array but each element is composed on key and value pair to store information
+        // key makes data unique, no two entries with same key
 
-        menu.put(1, new AltMenu("Swapper", () -> IntByReference.main(null) ) );
-        menu.put(2, new AltMenu("Matrix", () -> Matrix.main(null) ) );
+        // menu.put is creating a new element,
+        menu.put(1, new AltMenu("Swapper", () -> IntByReference.main(null) ) ); // options for the user to pick from
+        menu.put(2, new AltMenu("Matrix", () -> Matrix.main(null) ) ); // 2 is key, making a new class object
 
         System.out.println("Menu:");
-        for (Map.Entry<Integer, AltMenu> pair : menu.entrySet()) {
-            System.out.println(pair.getKey() + " ==> " + pair.getValue().getTitle());
+        // Map.Entry - access entries within a map
+        for (Map.Entry<Integer, AltMenu> pair : menu.entrySet()) { // entrySet returns a set that has the same elements as the map, entrySet is an array, take elements out
+            System.out.println(pair.getKey() + " ==> " + pair.getValue().getTitle()); // printing out key and value combination
         }
 
         int input = sc.nextInt();
-        AltMenu m = menu.get(input);
-        m.getAction().run();
+        AltMenu m = menu.get(input); // giving the input to menu.get - getting the action?
+        m.getAction().run(); // running the action
 
-        AltMenu.main(args);
+        AltMenu.main(args); // Recursively run Menu again
     }
 }
